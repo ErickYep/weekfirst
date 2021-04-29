0602069bc02d69852bbc0e0c2d3af62bb59e8e26
ErickYepremyan <epremyan_erik@mail.ru>
2021-04-29 11:53:05 +0400

    new numberBound.java file

---
+++ src/numberBound.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class numberBound {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter Your number - ");
+        int number = scanner.nextInt();
+        if(number>10 && number < 100)
+            System.out.println("From 10 to 100");
+        else if(number>200 && number<300)
+            System.out.println("From 200 to 300");
+        else if(number>300 && number<500)
+            System.out.println("From 300 to 500");
+        else
+            System.out.println("Out of Bounds");
+    }
+}
